5edd2cfc67aa3c23f67ca58a6455ad5ca90474f7
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2018-12-03 17:37:01 +0100

    Add controllers

---
+++ src/main/java/com/app/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.app.controllers;
+
+import com.app.model.entities.Gender;
+import com.app.model.entities.Patient;
+import com.app.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("patient", new Patient());
+        model.addAttribute("genders", Gender.values());
+        return "security/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(
+            @ModelAttribute Patient patient) {
+        userService.registerPatient(patient);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("error", "");
+        return  "security/loginForm";
+    }
+
+    @GetMapping("/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("error", "LOGIN DATA ERROR");
+        return "security/loginForm";
+    }
+
+    @GetMapping("/accessDenied")
+    public String accessDenied() {
+        return "security/accessDenied";
+    }
+
+}
